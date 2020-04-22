d3a40693852d9edfead8dccb84ef3fcb30ffdec0
wanlab-io <hello.wanlab@gmail.com>
2020-04-22 19:23:33 +0800

    add loops.js

---
+++ section-2/loops.js
@@ -0,0 +1,83 @@
+// FOR LOOP
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2){
+//     console.log('2 is my favourite number');
+//     continue;
+//   }
+
+//   if (i === 5){
+//     console.log('Stop the loop');
+//     break;
+//   }
+//   console.log('Number '+ i);
+// }
+
+// WHILE LOOP
+
+// let i = 0;
+
+// while(i < 10){
+//   console.log('Number' + i);
+//   i++;
+// }
+
+
+// DO WHILE
+
+// let i = 100;
+
+// do {
+//   console.log('Number ' + i);
+//   i++;
+// }
+
+// while( i < 10);
+
+
+
+// LOOP THROUGH ARRAY
+
+const cars = ['Ford','Chevy','Honda','Toyota'];
+
+// for(let i = 0; i < cars.length; i++) {
+//   console.log(cars[i]);
+// }
+
+// FOREACH .(always use foreach like this)
+// cars.forEach(function(car, index, array){
+//    console.log(`${index} : ${car}`);
+//    console.log(array);
+//  })
+
+
+// MAP
+// const users = [
+//   {id: 1, name:'John'},
+//   {id: 2, name:'Sara'},
+//   {id: 3, name:'Karen'},
+//   {id: 4, name:'Steve'} 
+// ];
+
+// const ids = users.map(function(user){
+//   return user.id;
+// })
+
+// console.log(ids);
+
+
+// FOR IN LOOP
+
+const user = {
+  firstName:'Hang',
+  lastName:'Jebat',
+  age:'40'
+}
+
+for ( let x in user ) {
+  console.log(`${x} : ${user[x]}`);
+} 
+
+
+
+
